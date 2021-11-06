8fefa293bf02a2d8d83bd499abde7a47d7cda7e2
Fisher Sun <fisher521.fs@gmail.com>
2021-11-06 18:40:45 -0500

    Add space to ribbon

--- init.zsh
+++ init.zsh
@@ -4,7 +4,7 @@ newline=$'\n'
 autoload -U colors && colors
 
 # The prompt proper
-PS1="${newline}%{$bg[blue]%}${dir}%{$fg[black]%}%{$reset_color%}${newline}"
+PS1="${newline}%{$bg[blue]%}${dir} %{$fg[black]%}%{$reset_color%}${newline}"
 
 # Underline command
 zle_highlight=(default:underline)

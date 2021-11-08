493ed88d4cf080fa80d51d6af221538d7fd73ff0
Fisher Sun <fisher521.fs@gmail.com>
2021-11-08 00:03:56 -0600

    Use bg as fg color for ribbon end
    This fixes the ugly ribbon end on many color schemes!

--- init.zsh
+++ init.zsh
@@ -1,6 +1,12 @@
+ribbon_color=4
+
 # Helper strings
 dir="%~"
 newline=$'\n'
+ribbon_bg=$(tput setab ${ribbon_color})
+# Set fg to bg color.
+# https://stackoverflow.com/a/24198187/14106506
+ribbon_end=$(tput sgr0; tput setaf ${ribbon_color}; tput smso)
 
 # Disable built-in virtualenv prompt
 # https://askubuntu.com/a/387098
@@ -16,5 +22,5 @@ function virtenv_indicator {
 add-zsh-hook precmd virtenv_indicator
 
 # The prompt proper
-PS1="${newline}%{$(tput setab 4)%}%(1V.(%1v) .)${dir} %{$(tput setaf 0)%}%{$(tput sgr0)%}${newline}$ "
+PS1="${newline}%{${ribbon_bg}%}%(1V.(%1v) .)${dir} %{${ribbon_end}%}%{$(tput sgr0)%}${newline}$ "
 
